b7f1877073167cb7930dc4f5411f323c61526d55
MarcTimperley <marctimperley@yahoo.co.uk>
2019-10-10 12:36:03 +0100

    new: add captions. fix:refactored animation

--- public/example/carousel.js
+++ public/example/carousel.js
@@ -1,4 +1,3 @@
-
 let canvas = {}
 let canvasWidth = 0
 let canvasHeight = 0
@@ -12,74 +11,52 @@ window.onload = function carousel() {
   canvasWidth = canvas.offsetWidth
   canvasHeight = canvas.offsetHeight
   console.log(canvasWidth, canvasHeight)
-  // canvas.style='width:100%;height:100%'
-  // const ctx = canvas.getContext('2d')
-  // canvas.addEventListener('mousedown', handleMouseDown)
-
-
-
-
-
   const sources = document.getElementById('sources')
-
-// let sourceFolder = fs.readDirSync(path.join(__dirname,'example','Images'))
-// for (let filename of sourceFolder) {
-  // loop folder
-/*
-if(filename.match(/.png/)){
-
-  console.log(filename)
-  let img = document.createElement('img')
-  img.src=filename
-  sources.appendChild(img)
-}
-}
-*/
-
   const sourceImages = [...sources.childNodes]
   for (const image of sourceImages) {
     if (image.nodeName === 'IMG') {
+      console.log(image)
       console.log(`loaded image ${image.alt}`)
       positionThumb(image)
     }
   }
   console.log(`all done`)
-  /*
-  const thumbs = [...canvas.childNodes]
-
-  for (let thumb of thumbs) {
-    thumb.addStyle()
-  }
-   */
 }
 
 function positionThumb(image) {
-  let imageSize = `width: ${Math.max(parseInt(Math.floor(canvasWidth * scale)), 180)}px; height:auto;`
-  if (image.naturalHeight > image.naturalWidth) imageSize = `width:auto; height:${Math.max(parseInt(Math.floor(canvasWidth * scale)), 180)}px;`
-  // const maxH = Math.max(parseInt(Math.floor(canvasHeight * scale / document.images.length)), 100)
-
+  let portrait = image.naturalHeight > image.naturalWidth ? true : false
+  let imageSize = {
+    width: Math.min(parseInt(Math.floor(canvasWidth * scale)), 280)+'px',
+    height: 'auto'
+  }
+  if (portrait) {
+    imageSize.width = 'auto'
+    imageSize.height = Math.min(parseInt(Math.floor(canvasWidth * scale)), 280)+'px'
+  }
+  //let imageSize = `width: ${Math.min(parseInt(Math.floor(canvasWidth * scale)), 280)}px; height:auto;`
+  //let containerSize = `width: ${Math.max(parseInt(Math.floor(canvasWidth * scale)), 180)}px;`
+  //if (image.naturalHeight > image.naturalWidth) imageSize = `width:auto; height:px;`
+  // if (image.naturalHeight > image.naturalWidth) containerSize = `width:auto; height:${Math.max(parseInt(Math.floor(canvasWidth * scale)), 180)}px;`
   const rotation = Math.floor(Math.random() * 50 - 25)
   const x = Math.floor(Math.random() * canvasWidth / 2)
   const y = Math.floor(Math.random() * canvasHeight / 2)
   // center better
   image.style = 'width:10px; height:10px; top:5px; left:5px; position:absolute;'
-  // add link
-  //  ctx.rotate(rotation * Math.PI / 180)
-  // preserve ratio
-  // ctx.fillStyle = '#ccc'
-  // ctx.fillRect(x - 5, y - 5, maxW + 10, maxH + 10)
   images.push({
     [image.src]: {
       x: x,
       y: y,
-      size: imageSize,
-      rotation: rotation
+      // size: imageSize,
+      rotation: rotation,
+      caption: image.alt
     }
   })
-  console.log(images)
-  // ctx.drawImage(image, x, y, maxW, maxH)
+  console.log()
   let html = `<label for="checkbox${count}"><input type="checkbox" id="checkbox${count}" class="check">`
-  html += `<img src="${image.src}" alt="${image.src}" class="thumbnail" id=${count} style="left:${x}px; top:${y}px; transform: rotate(${rotation}deg) scale(1); ${imageSize}">`
+  html += `<div class="image-container" style="transform: rotate(${rotation}deg) scale(1); ">`
+  html += `<img src="${image.src}" alt="${image.alt}" class="thumbnail" id=${count} style="width:${imageSize.width};height:${imageSize.height}">`
+  html += `<p class="caption" style="max-width:200px;">${image.alt}</p>`
+  html += `</div>`
   html += `</label>`
   canvas.innerHTML += html
   count++
